91792ea1de02d1ab59ac76f1ea12013105cbe8f5
mattkaneb1 <matthewkaneb1@gmail.com>
2020-05-14 22:55:04 -0400

    automated scraping of a list of tickers

--- yahoo_finance_web_scraping.R
+++ yahoo_finance_web_scraping.R
@@ -3,8 +3,10 @@ library(rvest)
 library(robotstxt)
 library(janitor)
 library(RSelenium)
+library(dplyr)
 
-ticker <- "AMZN"
+# Remove Dividend Rows before inner joining 
+ticker <- c("AMZN","GOOG","MSFT","FB")
 rn <- as.integer(as.POSIXct( Sys.time() ))
 
 #start RSelenium
@@ -14,7 +16,7 @@ remDr$open()
 
 #navigate to your page
 remDr$navigate(paste("https://finance.yahoo.com/quote/",
-                     ticker,
+                     ticker[1],
                      "/history?period1=",
                      as.character(rn - 315400000),
                      "&period2=",
@@ -22,15 +24,49 @@ remDr$navigate(paste("https://finance.yahoo.com/quote/",
                      "&interval=1d&filter=history&frequency=1d",sep=""))
 
 #scroll down 20 times, waiting for the page to load at each time
-for(i in 1:25){      
+for(i in 1:27){      
   remDr$executeScript(paste("scroll(0,",i*10000,");"))
   Sys.sleep(1)    
 }
 
+
 #Import into R
 page_source<-remDr$getPageSource()
-amzn_tables <- read_html(page_source[[1]]) %>% html_nodes("table")
-amzn10y <- html_table(amzn_tables[[1]])
+tables <- read_html(page_source[[1]]) %>% html_nodes("table")
+hist_data <- html_table(tables[[1]]) %>%
+  clean_names() %>%
+  select(date,adj_close) %>%
+  rename(!!ticker[1] := adj_close)
+
+
+ticker <- ticker[2:length(ticker)]
+# Repeat for the rest of the tickers
+#==========================================================================
+for(t in ticker){
+  remDr$navigate(paste("https://finance.yahoo.com/quote/",
+                       t,
+                       "/history?period1=",
+                       as.character(rn - 315400000),
+                       "&period2=",
+                       as.character(rn),
+                       "&interval=1d&filter=history&frequency=1d",sep=""))
+  
+  #scroll down 27 times, waiting for the page to load at each time
+  for(i in 1:27){      
+    remDr$executeScript(paste("scroll(0,",i*10000,");"))
+    Sys.sleep(1)    
+  }
+  
+  #Import into R
+  page_source<-remDr$getPageSource()
+  tables <- read_html(page_source[[1]]) %>% html_nodes("table")
+  hist_data2 <- html_table(tables[[1]]) %>%
+    clean_names() %>%
+    select(date,adj_close) %>%
+    rename(!!t := adj_close)
+  hist_data <- hist_data %>% inner_join(hist_data2,by="date")
+}
+#==========================================================================
 
 #Close Server
 remDr$close()
