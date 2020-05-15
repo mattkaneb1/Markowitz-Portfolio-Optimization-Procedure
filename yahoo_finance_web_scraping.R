abb2b2c78dabb3a982ec353534d43bb3c5624f62
mattkaneb1 <matthewkaneb1@gmail.com>
2020-05-15 11:46:50 -0400

    Stats done, just need to optimize now

--- yahoo_finance_web_scraping.R
+++ yahoo_finance_web_scraping.R
@@ -4,8 +4,8 @@ library(robotstxt)
 library(janitor)
 library(RSelenium)
 library(dplyr)
+library(lubridate)
 
-# Remove Dividend Rows before inner joining 
 ticker <- c("AMZN","GOOG","MSFT","FB")
 rn <- as.integer(as.POSIXct( Sys.time() ))
 
@@ -26,7 +26,7 @@ remDr$navigate(paste("https://finance.yahoo.com/quote/",
 #scroll down 20 times, waiting for the page to load at each time
 for(i in 1:27){      
   remDr$executeScript(paste("scroll(0,",i*10000,");"))
-  Sys.sleep(1)    
+  Sys.sleep(.25)    
 }
 
 
@@ -35,14 +35,15 @@ page_source<-remDr$getPageSource()
 tables <- read_html(page_source[[1]]) %>% html_nodes("table")
 hist_data <- html_table(tables[[1]]) %>%
   clean_names() %>%
-  select(date,adj_close) %>%
+  select(date,adj_close)%>%
+  filter(!grepl("Dividend",adj_close,ignore.case=TRUE)) %>%
   rename(!!ticker[1] := adj_close)
 
 
-ticker <- ticker[2:length(ticker)]
+ticker2 <- ticker[2:length(ticker)]
 # Repeat for the rest of the tickers
 #==========================================================================
-for(t in ticker){
+for(t in ticker2){
   remDr$navigate(paste("https://finance.yahoo.com/quote/",
                        t,
                        "/history?period1=",
@@ -63,8 +64,9 @@ for(t in ticker){
   hist_data2 <- html_table(tables[[1]]) %>%
     clean_names() %>%
     select(date,adj_close) %>%
+    filter(!grepl("Dividend",adj_close,ignore.case=TRUE)) %>%
     rename(!!t := adj_close)
-  hist_data <- hist_data %>% inner_join(hist_data2,by="date")
+  hist_data <- hist_data %>% inner_join(hist_data2)
 }
 #==========================================================================
 
@@ -72,5 +74,34 @@ for(t in ticker){
 remDr$close()
 rD[["server"]]$stop()
 gc(rD)
+
+pct <- function(x) { (x[1]-x[2])/x[2] * 100}
+parse<- function(x) {as.numeric(gsub(",","",x))}
+
+wr_hist_data <- hist_data %>% 
+  mutate(date = mdy(date), year = year(date),month=month(date),day = day(date)) %>%
+  group_by(year,month) %>%
+  filter(day == max(day) | day == min(day)) %>%
+  mutate_at(ticker,parse) %>%
+  summarize_at(ticker,pct)
+
+annualize_ror <- function(x) {mean(x)*12}
+annualize_sd <- function(x) {sd(x)*sqrt(12)}
+
+annualized_monthly_ror <- wr_hist_data %>%
+  ungroup() %>%
+  group_by(year) %>%
+  summarize_at(ticker,annualize_ror) 
+
+annualized_monthly_sd <- wr_hist_data %>%
+  ungroup() %>%
+  group_by(year) %>%
+  summarize_at(ticker,annualize_sd)
+
+covar <- wr_hist_data %>% 
+  ungroup() %>%
+  select(-c(year,month)) %>%
+  cov()
+  
 
 
